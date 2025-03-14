056bae535c3c8ad2cd34b9e30f83a8802f2b9c16
yokoi-ryotaro <ryotaro.y0603@gmail.com>
2025-03-14 16:46:31 +0900

    初回

---
+++ src/main/java/com/example/demo/ECsiteCraftBeerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECsiteCraftBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECsiteCraftBeerApplication.class, args);
+	}
+
+}
